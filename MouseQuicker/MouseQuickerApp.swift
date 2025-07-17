3b52fb4355d2019dfa26910da5c1deba689a7463
syferie <syferie@gmail.com>
2025-07-17 16:47:42 +0800

    Initial Commit

---
+++ MouseQuicker/MouseQuickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseQuickerApp.swift
+//  MouseQuicker
+//
+//  Created by syferie on 2025/7/17.
+//
+
+import SwiftUI
+
+@main
+struct MouseQuickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MouseQuickerTests/MouseQuickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MouseQuickerTests.swift
+//  MouseQuickerTests
+//
+//  Created by syferie on 2025/7/17.
+//
+
+import Testing
+@testable import MouseQuicker
+
+struct MouseQuickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
